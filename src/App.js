289c6e0d6d603ec8d055328a088ca7d40f3d2715
Kristin Lam <klam623@gmail.com>
2022-08-07 15:36:11 -0400

    remove unneeded files, modify App and index files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <p>Hello, I'm Kristin</p>
+        <About />
+        <Projects />
+        <Contact />
+      </div>
     </div>
   );
 }
