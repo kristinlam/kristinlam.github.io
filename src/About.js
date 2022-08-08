40193acb3dc8cbfbeab4a2a00ea147158dd12986
Kristin Lam <klam623@gmail.com>
2022-08-08 01:06:34 -0400

    add color variables

--- src/About.js
+++ src/About.js
@@ -5,12 +5,12 @@ import aboutImg from './images/me.jpg';
 const About = () => {
   return (
     <section id="about">
-      <div className={styles.heading}>
-        <h2>01. About Me</h2>
-        <h1>Words Here</h1>
-      </div>
       <div className={styles.about}>
         <div className={styles.description}>
+          <div className={styles.heading}>
+            <h2>01. About Me</h2>
+            <h1>Words Here</h1>
+          </div>
           <p>
             Hello! My name is Kristin and I love creating things on the web. I
             first fell in love with web development in 2005, and would rush home
@@ -29,31 +29,31 @@ const About = () => {
             algorithms and data structures, you can find me crocheting, reading
             or drinking matcha lattes.
           </p>
-          <div className={styles.skills}>
-            <div className={styles.column}>
-              <h3>Languages</h3>
-              <ul>
-                <li>JavaScript (ES6)</li>
-                <li>HTML5</li>
-                <li>CSS3</li>
-              </ul>
-            </div>
-            <div className={styles.column}>
-              <h3>Tools & Technologies</h3>
-              <ul>
-                <li>React</li>
-                <li>Redux</li>
-                <li>Express.js</li>
-                <li>Sequelize.js</li>
-                <li>PostgreSQL</li>
-                <li>React Native</li>
-                <li>Git & Github</li>
-              </ul>
-            </div>
-          </div>
         </div>
         <div className={styles.photo}>
           <img src={aboutImg} />
+        </div>
+      </div>
+      <div className={styles.skills}>
+        <div className={styles.column}>
+          <h3>Languages</h3>
+          <ul>
+            <li>JavaScript (ES6)</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h3>Tools & Technologies</h3>
+          <ul>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Express.js</li>
+            <li>Sequelize.js</li>
+            <li>PostgreSQL</li>
+            <li>React Native</li>
+            <li>Git & Github</li>
+          </ul>
         </div>
       </div>
     </section>
