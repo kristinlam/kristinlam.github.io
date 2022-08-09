67ef53684380cc34d1280bf4f44f6d6f7997e2e7
Kristin Lam <klam623@gmail.com>
2022-08-09 02:02:32 -0400

    add projects section with images

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import projects from './assets/projects';
+import styles from './styles/projects.module.scss';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section id="projects" className={styles.projectsSection}>
+      <h2>Projects</h2>
+      <div>
+        {projects.map((project) => (
+          <div className={styles.project} key={project.id}>
+            <div>
+              <h3>
+                0{project.id}. {project.title}
+              </h3>
+              <img
+                alt="project"
+                src={require(`./assets/images/${project.image}`)}
+              />
+            </div>
+            <div className={styles.projectInfo}>
+              <div className={styles.description}>
+                {project.description.map((paragraph) => (
+                  <p>{paragraph}</p>
+                ))}
+              </div>
+
+              <p className={styles.tech}>{project.tech}</p>
+              <a href={project.link} rel="noopener noreferrer" target="_blank">
+                view the project
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
---
+++ src/assets/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Okidoki',
+    tech: 'React Native, Expo, React Hooks, React Native Health, React Query, Express, Sequelize, Postgres, Styled Components',
+    description: [
+      'A mobile application connected to the Apple Healthkit that encourages you to be active. Meeting daily step goals rewards you with in-game currency, allowing you to feed and play with your pet.',
+      'I built out the store component, ability to increase your pet’s mood with toys, and general styling of all screens. I also worked on authentication using Bcrypt, JSON Web Tokens, and Expo Secure Store. ',
+    ],
+    image: 'gumbo.jpg',
+    link: 'https://github.com/CharismaticCarrots/Okidoki',
+  },
+  {
+    id: 2,
+    title: 'Gumbo NYC',
+    tech: 'Bootstrap, HTML, CSS, JavaScript',
+    description: [
+      'I designed and created the Gamers Under the Manhattan Bridge Overpass website for a collective of indie game studios in NYC.',
+    ],
+    image: 'gumbo.jpg',
+    link: 'https://www.gumbo.nyc/',
+  },
+  {
+    id: 3,
+    title: 'The Bowery Mission',
+    tech: 'HTML, CSS, Foundation',
+    description: [
+      "As the digital development coordinator for The Bowery Mission, NYC's oldest homeless shelter, I spurred the complete website overhaul of bowery.org in April 2018 by coding and debugging hundreds of pages for the flagship site.",
+    ],
+    image: 'tbm.jpg',
+    link: 'https://www.bowery.org/',
+  },
+];
+
+export default projects;
